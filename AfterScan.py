cbc8eacaa56b96446bc5a68bf4806a064f3414e3
jareff-g <juan.remirezdeesparza@gmail.com>
2025-12-05 19:19:10 +0100

    Refactoring: Move template classes to external file

--- AfterScan.py
+++ AfterScan.py
@@ -20,10 +20,10 @@ __copyright__ = "Copyright 2022-25, Juan Remirez de Esparza"
 __credits__ = ["Juan Remirez de Esparza"]
 __license__ = "MIT"
 __module__ = "AfterScan"
-__version__ = "1.40.06"
+__version__ = "1.40.07"
 __data_version__ = "1.0"
 __date__ = "2025-12-05"
-__version_highlight__ = "Refactoring: Remove code factorized in retrieve_dict_value_with_key_backward_compatibility"
+__version_highlight__ = "Refactoring: Move template classes to external file"
 __maintainer__ = "Juan Remirez de Esparza"
 __email__ = "jremirez@hotmail.com"
 __status__ = "Development"
@@ -78,6 +78,8 @@ except ImportError:
     requests_loaded = False
 
 from define_rectangle import DefineRectangle
+
+from template_manager import Template, TemplateList
 
 # Check for temporalDenoise in OpenCV at startup
 HAS_TEMPORAL_DENOISE = hasattr(cv2, 'temporalDenoising')
@@ -370,168 +372,6 @@ last_displayed_image = 0
 active_threads = 0
 num_threads = 0
 
-
-"""
-#################
-Classes
-#################
-"""
-class Template:
-    def __init__(self, name, filename, type, position, width):
-        self.name = name
-        self.filename = filename
-        self.type = type
-        if self.type == 'custom':
-            self.scale = 1.0
-        else:
-            self.scale = width/2028
-        self.position = position
-        self.scaled_position = (int(self.position[0] * self.scale),
-                                int(self.position[1] * self.scale))
-        self.size = (0,0)
-        if os.path.isfile(filename):
-            self.template = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
-            self.scaled_template = resize_image(self.template, self.scale)
-            # Calculate the white on black proportion to help with detection
-            self.white_pixel_count = cv2.countNonZero(self.scaled_template)
-            total_pixels = self.scaled_template.size
-            self.wb_proportion = self.white_pixel_count / total_pixels
-            self.size = (self.template.shape[1],self.template.shape[0])
-            self.scaled_size = (int(self.size[0] * self.scale),
-                                int(self.size[1] * self.scale))
-            logging.debug(f"Init Template {self.type}, size: {self.size}, scaled size: {self.scaled_size}")
-        else:
-            self.template = None
-            self.scaled_template = None
-            self.wb_proportion = 0.5
-            self.size = (0,0)
-            self.scaled_size = (0,0)
-
-    def refresh(self, width):
-        if self.type == 'custom':
-            self.scale = 1.0
-        else:
-            self.scale = width/2028
-        self.template = cv2.imread(self.filename, cv2.IMREAD_GRAYSCALE)
-        self.scaled_template = resize_image(self.template, self.scale)
-        self.white_pixel_count = cv2.countNonZero(self.scaled_template)
-        total_pixels = self.scaled_template.size
-        self.wb_proportion = self.white_pixel_count / total_pixels
-        self.size = (self.template.shape[1], self.template.shape[0])
-        self.scaled_size = (int(self.size[0] * self.scale),
-                            int(self.size[1] * self.scale))
-        self.scaled_position = (int(self.position[0] * self.scale),
-                                int(self.position[1] * self.scale))
-        logging.debug(f"Init Template {self.type}, size: {self.size}, scaled size: {self.scaled_size}")
-
-
-class TemplateList:
-    def __init__(self):
-        self.templates = []
-        self.active_template = None  # Initialize active_element to None
-        self.img_width = 2028
-        self.img_height = 1520
-
-    def add(self, name, filename, type, position):
-        exists = False
-        for t in self.templates:
-            if t.name == name and t.type == type:  # If already exist, update it
-                self.active_template = t
-                exists = True
-                break
-        if exists:
-            t.filename = filename
-            t.position = position
-            t.refresh(self.img_width)
-            target = t
-        else:
-            target = Template(name, filename, type, position, self.img_width)
-            self.templates.append(target)
-        self.active_template = target   # Set template just added as active
-        return target
-
-    def get_all(self):
-        return self.templates
-
-    def remove(self, template):
-        if template in self.templates:
-            self.templates.remove(template)
-            if template == self.active_template:
-                self.active_template = None  # Reset active_element if removed
-            return True
-        else:
-            return False
-
-    def set_active(self, type, name):
-        for t in self.templates:
-            if t.type == type and t.name == name:
-                self.active_template = t
-                return True
-        return False
-
-    def get_template(self, type, name):
-        for t in self.templates:
-            if t.type == type and t.name == name:
-                return t.scaled_template
-        return None
-
-    def get_active(self):
-        return self.active_template
-
-    def get_active_template(self):
-        return self.active_template.scaled_template
-
-    def get_active_name(self):
-        return self.active_template.name
-
-    def get_active_position(self):
-        return self.active_template.scaled_position
-
-    def set_active_position(self, position):
-        self.active_template.scaled_position = position
-        self.active_template.position =  (int(t.scaled_position[0] / self.active_template.scale),
-                                    int(t.scaled_position[1] / self.active_template.scale))
-    def get_active_size(self):
-        return self.active_template.scaled_size
-
-    def set_active_size(self, size):
-        self.active_template.scaled_size = size
-        self.active_template.size = (int(size[0] / self.active_template.scale),
-                                int(size[1] / self.active_template.scale))
-    def get_scale(self):
-        # Size reference 2028x1520
-        return self.active_template.scale   # Scale is dynamic, as it depends on the set of images currently loaded
-
-    def set_scale(self, sample_img):
-        # If new image size is different, Update all scaled templates and positions
-        # Size reference 2028x1520
-        # Probably we could call t.refresh instead of the following code, but let's keep it as is for now
-        img_width = sample_img.shape[1]
-        img_height = sample_img.shape[0]
-        new_scale = img_width/2028
-        for t in self.templates:
-            if t.type != 'custom' and new_scale != t.scale:
-                t.scale = new_scale
-                t.scaled_position = (int(t.position[0] * new_scale),
-                                    int(t.position[1] * new_scale))
-                t.scaled_template = resize_image(t.template, new_scale)
-                t.scaled_size = (int(t.size[0] * new_scale),
-                                int(t.size[1] * new_scale))
-
-    def get_active_filename(self):
-        return self.active_template.filename
-
-    def get_active_type(self):
-        return self.active_template.type
-
-    def get_active_white_pixel_count(self):
-        return self.active_template.white_pixel_count
-
-    def get_active_wb_proportion(self):
-        return self.active_template.wb_proportion
-
-    def set_active_wb_proportion(self, proportion):
-        self.active_template.wb_proportion = proportion
 
 
 
